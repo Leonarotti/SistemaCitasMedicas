3ab90ef9e68c95ba725acdcee0762baf68e9b765
Leonardo Pereira <159678359+Leonarotti@users.noreply.github.com>
2025-06-04 20:40:36 -0600

    Pruebas

--- JS/menu.js
+++ JS/menu.js
@@ -6,21 +6,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
     header.innerHTML = `<div class="content">
 <div class="menu container">
-    <a href="/index.html" class="logo">
+    <a href="index.html" class="logo">
         Clínica Árbol de Seda
     </a>
     <input type="checkbox" id="menu">
     <label for="menu">
-        <img class="menu-icono" src="/assets/menu.svg" alt="menu">
+        <img class="menu-icono" src="assets/menu.svg" alt="menu">
     </label>
     <nav class="navbar">
         <ul>
-            <li><a href="/index.html">Inicio</a></li>
-            <li><a href="/HTML-Pages/agenda.html">Agenda de citas</a></li>
-            <li><a href="/HTML-Pages/medics.html">Médicos</a></li>
-            <li><a href="/HTML-Pages/nosotros.html">Sobre nosotros</a></li>
-            <li><a href="/HTML-Pages/servicios.html">Servicios</a></li>
-            <li><a id="login" class="btn-login" href="/HTML-Pages/login.html">Iniciar Sesión</a></li>
+            <li><a href="index.html">Inicio</a></li>
+            <li><a href="HTML-Pages/agenda.html">Agenda de citas</a></li>
+            <li><a href="HTML-Pages/medics.html">Médicos</a></li>
+            <li><a href="HTML-Pages/nosotros.html">Sobre nosotros</a></li>
+            <li><a href="HTML-Pages/servicios.html">Servicios</a></li>
+            <li><a id="login" class="btn-login" href="HTML-Pages/login.html">Iniciar Sesión</a></li>
             <li><button id="logout" class="btn-login" onclick="logout()">Cerrar Sesión</button></li>
         </ul>
     </nav>
@@ -28,11 +28,11 @@ document.addEventListener("DOMContentLoaded", function () {
 </div>`;
 
     footer.innerHTML = `<p>&copy; 2024 Clínica Árbol de Seda - Derechos reservados</p>
-<a href="/HTML-Pages/faq.html">Preguntas frecuentes</a>
+<a href="HTML-Pages/faq.html">Preguntas frecuentes</a>
 <p class="hid">|</p>
-<a href="/HTML-Pages/contacto.html">Contacto</a>
+<a href="HTML-Pages/contacto.html">Contacto</a>
 <p class="hid">|</p>
-<a href="/HTML-Pages/politicas.html">Política de privacidad & Términos de uso</a>
+<a href="HTML-Pages/politicas.html">Política de privacidad & Términos de uso</a>
 `;
 
     if (user) {
@@ -45,5 +45,5 @@ const logout = () => {
     alert('Hasta pronto!');
     localStorage.removeItem('login_success');
     document.getElementById('logout').style.display = 'none'
-    window.location.href = '/index.html';
+    window.location.href = 'index.html';
 };
